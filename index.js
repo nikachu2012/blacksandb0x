082afc3e82ebe81a20835bdd61aeeb8fb7945797
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-01-24 23:51:27 +0900

    [add] debugの追加

--- index.js
+++ index.js
@@ -13,9 +13,36 @@ const listener = app.listen(80, function () {
 app.get('/view', function (req, res) {
     let send = [];
     // debug
+    if(req.query.debug === "true"){
+        send.push('<pre><code >')
+        send.push(`hackSandb0x debug\n`)
+        send.push(`load Date: ${new Date()}\n`)
+        send.push(`input Data: ${req.query.d}\n`)
+        send.push(`convert Data: ${htmlspecialchars(Buffer.from(decodeURIComponent(req.query.d), 'base64').toString())}\n`)
+        send.push('</code></pre>')
+    }
+    
     const data = Buffer.from(decodeURIComponent(req.query.d), 'base64').toString();
     send.push(data)
 
     res.send(send.join(''))
 });
 
+function htmlspecialchars(unsafeText){
+    if(typeof unsafeText !== 'string'){
+      return unsafeText;
+    }
+    return unsafeText.replace(
+      /[&'`"<>]/g, 
+      function(match) {
+        return {
+          '&': '&amp;',
+          "'": '&#x27;',
+          '`': '&#x60;',
+          '"': '&quot;',
+          '<': '&lt;',
+          '>': '&gt;',
+        }[match]
+      }
+    );
+  }
